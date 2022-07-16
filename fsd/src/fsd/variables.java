d49799cf23886066b0cee6ad3d4e722070a7c072
uvk37 <vijaykumarulasa@gmail.com>
2022-07-16 17:55:26 +0530

    changes

---
+++ fsd/src/fsd/variables.java
@@ -0,0 +1,23 @@
+package fsd;
+public class variables {
+
+	public static void main(String[] args) {
+		int myint = 5;
+		System.out.println(myint);
+		
+		myint = 10 + 5;
+		System.out.println(myint);
+		
+		myint = 10 + (5 + 2) * 10;
+		System.out.println(myint);
+		
+		int myFirstNumber = 5;
+		int mySecondNumber = 10;
+		int myThirdNumber = 15;
+		
+		myint = myFirstNumber + mySecondNumber + myThirdNumber;
+		System.out.println(myint);
+		
+	}
+	
+}
