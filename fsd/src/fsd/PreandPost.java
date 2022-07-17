e2aecdbefe4142a6fcfd7603f8f468c5112d278b
uvk37 <vijaykumarulasa@gmail.com>
2022-07-17 12:36:05 +0530

    changes

---
+++ fsd/src/fsd/PreandPost.java
@@ -0,0 +1,25 @@
+package fsd;
+
+public class PreandPost {
+
+	public static void main(String[] args) {
+int i = 3;
+		
+		int a = i++;
+		
+		System.out.println("a = " + a);
+		System.out.println("i = " + i);
+		
+//		int b = i;
+		
+		String x = null;
+		if (x!=null) {
+			System.out.println("x has a value");
+		} else {
+			System.out.println("x does not have a value");
+		}
+		
+
+	}
+
+}
