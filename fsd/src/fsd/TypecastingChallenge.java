ccca7be9765cb9b1e40dee55d74490c51f7e8cc8
uvk37 <vijaykumarulasa@gmail.com>
2022-07-19 18:11:20 +0530

    changes

--- fsd/src/fsd/TypecastingChallenge.java
+++ fsd/src/fsd/TypecastingChallenge.java
@@ -7,17 +7,17 @@ String input = "12";
 		
 		// convert the string input above into appropriate primitive type
 		
-//		byte mybyte = Byte.parseByte(input);
-//		System.out.println("Byte val =" + mybyte);
-//		
-//		int myint = Integer.parseInt(input);
-//		System.out.println("Integer val = " + myint);
+		byte mybyte = Byte.parseByte(input);
+	    System.out.println("Byte val =" + mybyte);
+		
+		int myint = Integer.parseInt(input);
+		System.out.println("Integer val = " + myint);
 
-//		Double d = Double.parseDouble(input);
-//		System.out.println(d);
-//		
-//		int i  = Integer.valueOf(input);
-//		System.out.println(i);
+		Double d = Double.parseDouble(input);
+		System.out.println(d);
+		
+    	int i  = Integer.valueOf(input);
+    	System.out.println(i);
 		
 		char input2 = (char)Integer.parseInt(input); 
 		System.out.println(input2);
